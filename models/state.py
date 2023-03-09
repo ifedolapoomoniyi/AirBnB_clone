ea3994e19d43240c54fa1aa86f1f096407cd1143
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2023-03-09 13:52:10 -0800

    class for state

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a class State"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Defines State with the atrribute name"""
+
+    name = ""
